15fb320f39809cccb0b537a88e3225fd7822fec0
perfildoalan <enviarparaalan@gmail.com>
2025-02-19 15:05:17 -0300

    changes into header

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
+import MenuBurg from "@/public/Icons/Menu.svg";
+
 export default function Header() {
     return (
         <header>
-            <div className="w-screen h-[80px] bg-conecta-azul">
-                Barra superior
+            <div className="w-screen h-[80px] bg-conecta-azul p-[10px]">
+                <Image 
+                    src={MenuBurg} 
+                    alt="Icone de Menu"
+                    className="w-[50px] transition-all duration-500 hover:invert-[0.20]"
+                />
             </div>
         </header>
     )
