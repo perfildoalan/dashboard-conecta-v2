ee79329d8412c2cb3d886e286ef1cea3e42581d6
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2025-02-20 10:15:38 -0300

    newPassword

---
+++ app/(home)/newPassword/action/newPassword.action.ts
@@ -0,0 +1,34 @@
+import { NewPasswordFormState, NewPasswordFormSchema } from '@/lib/newPassword'
+import { api } from '@/utils/api'
+
+export async function newPasswordAction(
+  state: NewPasswordFormState,
+  formData: FormData,
+) {
+  const validateField = NewPasswordFormSchema.safeParse({
+    password: formData.get('password'),
+    confirmPassword: formData.get('confirmPassword'),
+  })
+
+  if (!validateField.success) {
+    return {
+      errors: validateField.error.flatten().fieldErrors,
+    }
+  }
+
+  const { password, confirmPassword } = validateField.data
+
+  api
+    .post('/new-password', { password, confirmPassword })
+    .then(() => {
+      if (state) {
+        state.message = 'Password reset email sent'
+      }
+      return state
+    })
+    .catch(() => {
+      return { error: 'Invalid email' }
+    })
+
+  return state
+}
