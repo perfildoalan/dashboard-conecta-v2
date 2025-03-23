a6496df87b865cc8d66b1e9004fd8acccea90227
perfildoalan <enviarparaalan@gmail.com>
2025-03-23 16:58:10 -0300

    fix grid position

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -18,14 +18,14 @@ export default function Header() {
 
     return (
         <header>
-            <div className="grid grid-cols-3 max-md:grid-cols-4 max-[400px]:grid-cols-2 z-[1000] md:gap-[5vw] justify-between justify-items-center items-center w-screen h-[80px] bg-header shadow-[0px_8px_8px_rgba(0,0,0,0.10)]">
+            <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-3 z-[1000] md:gap-[5vw] justify-between justify-items-center items-center w-screen h-[80px] bg-header shadow-[0px_8px_8px_rgba(0,0,0,0.10)]">
                 <div className='flex ml-4 gap-4 justify-start justify-self-start items-center'>
                     <SidebarProvider>
                         <SheetDemo/>
                     </SidebarProvider>
                     <ConectaLogo color={(theme === 'dark' ? '#70E5FF' : 'white')} className="justify-self-start block transition-all max-md:hidden duration-300 hover:opacity-50 cursor-pointer" />
                 </div>
-                <div className='flex flex-row max-md:col-span-2 relative max-[400px]:hidden justify-center justify-items-center items-center w-full max-w-[500px]'>
+                <div className='flex flex-row max-md:col-span-2 relative max-sm:hidden justify-center justify-items-center items-center w-full max-w-[500px]'>
                     <input type="text" id="site-search" className="pl-4 pr-10 rounded-3xl bg-transparent text-headerItens text-lg focus:outline-none border-2 border-solid border-headerItens w-full" />
                     <Search className='absolute right-4 transition-all cursor-pointer text-headerItens hover:opacity-50'/>
                 </div>
