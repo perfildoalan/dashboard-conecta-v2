62c941d9a2e497b397d3d9b5ee0f9822ca4e478f
perfildoalan <enviarparaalan@gmail.com>
2025-03-25 14:07:32 -0300

    vehicle component

--- app/dashboard/vehicles/page.tsx
+++ app/dashboard/vehicles/page.tsx
@@ -1,4 +1,20 @@
+import VehicleCard from "./ui/vehicle-card";
+
 export default function Vehicles() {
-    return <h1>Veiculos</h1>
+    return (
+    <>
+    <h1>Veiculos</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+      <VehicleCard color="black" brand="Chevrolet" model="Prisma" year={2011} category="Seda" motor="1.4">
+      </VehicleCard>
+      <VehicleCard color="green" brand="Chevrolet" model="Prisma" year={2011} category="Seda" motor="1.4">
+      </VehicleCard>
+      <VehicleCard color="blue" brand="Chevrolet" model="Prisma" year={2011} category="Seda" motor="1.4">
+      </VehicleCard>
+      <VehicleCard color="yellow" brand="Chevrolet" model="Prisma" year={2011} category="Seda" motor="1.4">
+      </VehicleCard>
+    </div>
+    </>
+  )
   }
   
